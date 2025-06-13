57e1c28f9355075894d72fe07f9c3656b9844fd3
Pranav Puranam <puranampranav6@gmail.com>
2025-06-13 15:14:41 +0100

    Finished
    Implemented basic statistical tests

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
 
 # STEP 2: READ IN RAW DATA
 
@@ -32,4 +34,56 @@ y = est_data["MSFT Excess Return"] # This is what we're trying to get out
 model = sm.OLS(y, X).fit()
 
 print(model.summary())
+
+# STEP 6: DEFINE THE WINDOW OF THE EVENT ITSELF
+
+k_back = 0 # How many days before the announcement are we looking at, i.e. did news leak early
+k_front = 9 # How many days after the announcement are we looking at, i.e. how long did event impact returns
+
+evt_start = event_date - pd.Timedelta(days = k_back)
+evt_end = event_date + pd.Timedelta(days = k_front)
+
+evt_data = msft.loc[evt_start:evt_end].copy()
+
+# STEP 7: MAKE PREDICTIONS USING REGRESSION MODEL
+
+X_evt = sm.add_constant(evt_data[[
+    "Market Excess Return (Mkt-RF)",
+    "Size Factor (SMB)",
+    "Value Factor (HML)"
+]])
+
+evt_data["Expected"] = model.predict(X_evt)
+
+# STEP 8: CALCULATE ABNORMAL RETURNS AND OUTPUT
+
+evt_data["Abnormal"] = evt_data["MSFT Excess Return"] - evt_data["Expected"]
+evt_data["CAR"] = evt_data["Abnormal"].cumsum()
+
+print(evt_data[["MSFT Excess Return", "Expected", "Abnormal", "CAR"]])
+print(f"\nCumulative Abnormal Return (CAR) from {evt_start.date()} to {evt_end.date()}: {evt_data['Abnormal'].sum():.4%}")
+
+# STEP 9: CONDUCT A T TEST
+
+X_est = sm.add_constant(est_data[[
+    "Market Excess Return (Mkt-RF)",
+    "Size Factor (SMB)",
+    "Value Factor (HML)"
+]])
+est_data["Expected"] = model.predict(X_est)
+est_data["Residual"] = est_data["MSFT Excess Return"] - est_data["Expected"]
+
+sigma_AR = est_data["Residual"].std()
+
+CAR = evt_data["Abnormal"].sum()
+n = len(evt_data)
+
+t_stat = CAR / (sigma_AR * np.sqrt(n))
+
+print(f"\nT-statistic for CAR: {t_stat:.2f}")
+
+df = len(est_data) - 4  # 3 betas + intercept
+
+p_val = 2 * (1 - stats.t.cdf(abs(t_stat), df))
+print(f"P-value: {p_val:.4f}")
 
